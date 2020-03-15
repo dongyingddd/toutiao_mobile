37d4de1a641bc8d2ebb250e4ee8ca79fe1adf992
dongying <2273010094@qq.com>
2020-03-15 19:11:29 +0800

    vant的安装和使用

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import vant from 'vant'
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
-
+Vue.use(vant) // 全局注册 实际上调用了vant到处的对象的install方法
 new Vue({
   router,
   store,
