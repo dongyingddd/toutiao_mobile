f079d66ae738e394a0bcf75a143f62be65f23847
dongying <2273010094@qq.com>
2020-03-15 22:06:02 +0800

    完成layout组件并引入全局样式

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vant from 'vant'
 import 'vant/lib/index.css'
 import 'amfe-flexible' // 修改rem基准值的js插件
+import '@/styles/index.less' // 引入全局自定义样式 覆盖vant的样式
 Vue.config.productionTip = false
 Vue.use(vant) // 全局注册 实际上调用了vant到处的对象的install方法
 new Vue({
