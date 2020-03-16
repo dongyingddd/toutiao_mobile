0f8dea893ecf4f51e1b2eb6f98efb8cce7cff2ed
dongying <2273010094@qq.com>
2020-03-16 22:25:55 +0800

    完成登录页面的基础布局

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import vant from 'vant'
 import 'vant/lib/index.css'
 import 'amfe-flexible' // 修改rem基准值的js插件
 import '@/styles/index.less' // 引入全局自定义样式 覆盖vant的样式
+import '@/permission' // 引入导航守卫
+import '@/utils/request' // 引入封装后的请求管理工具request
 Vue.use(vant)
 Vue.config.productionTip = false
 // 全局注册 实际上调用了vant到处的对象的install方法
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,6 +4,17 @@
     <!-- click-left点击左侧事件 -->
     <!-- $router.go(-1) $router.back() -->
     <van-nav-bar title='登录'  left-arrow @click-left="$router.back()"></van-nav-bar>
+    <!-- 登陆布局 -->
+    <van-cell-group>
+      <van-field label="手机号" placeholder="请输入手机号"></van-field>
+      <van-field label="验证码" placeholder="请输入验证码">
+        <van-button slot="button" type="primary" size="small">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+     <div class="login-box">
+        <van-button type="info" block round size="small">登录</van-button>
+     </div>
   </div>
 </template>
 
@@ -13,6 +24,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .login-box{
+    padding: 20px;
+  }
 </style>
