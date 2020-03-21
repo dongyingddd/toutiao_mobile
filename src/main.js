3b29c5377db356a990f8352318a60f34019d6fac
dongying <2273010094@qq.com>
2020-03-21 17:52:14 +0800

    实现图片懒加载

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vant from 'vant'
+import vant, { Lazyload } from 'vant' // 导入Lazyload 实现图片懒加载
 // import 'vant/lib/index.css'
 import 'vant/lib/index.less'
 import 'amfe-flexible' // 修改rem基准值的js插件
@@ -12,6 +12,7 @@ import '@/utils/request' // 引入封装后的请求管理工具request
 import plugin from '@/utils/plugin'
 Vue.use(vant)
 Vue.use(plugin)
+Vue.use(Lazyload) // 全局注册v-lazy指令
 Vue.config.productionTip = false
 // 全局注册 实际上调用了vant到处的对象的install方法
 new Vue({
