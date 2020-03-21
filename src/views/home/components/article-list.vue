c24216ea44011461f8afdfee86fc0db25255a363
dongying <2273010094@qq.com>
2020-03-21 14:43:02 +0800

    article-list实现下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -6,11 +6,13 @@
   <div class="scroll-wrapper">
       <!-- 文章列表 -->
       <!-- van-list组件 如果不加干涉,初始化完毕后,就会检测自己距离底部的长度,如果超过了限定,就会执行load事件 自动把绑定的loadind变为true -->
-      <van-list v-model="upLoading" :finished="finished" @load="onload" finished-text="没有了">
+    <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="successText">
+          <van-list v-model="upLoading" :finished="finished" @load="onload" finished-text="没有了">
           <van-cell-group>
               <van-cell title="美股又熔断了" :value="`天台排队${item}`" v-for="item in articles" :key="item"></van-cell>
           </van-cell-group>
       </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -20,10 +22,13 @@ export default {
     return {
       upLoading: false, // 表示是否开启了上拉加载
       finished: false, // 表示是否已经完成所有数据的加载
-      articles: [] // 定义一个数组
+      articles: [], // 定义一个数组
+      downLoading: false, // 下拉刷新状态 表示是否正在下拉刷新
+      successText: ''// 刷新成功的文本
     }
   },
   methods: {
+    // 上拉加载
     onload () {
       console.log('开始加载数据')
       // 如果你有数据,应该把数据加到list中
@@ -41,6 +46,19 @@ export default {
       //   setTimeout(() => {
       //     this.finished = true // 表示数据已经加载完毕了 没有数据了
       //   }, 1000)
+    },
+
+    // 下拉刷新
+    onRefresh () {
+      setTimeout(() => {
+        // 模拟下拉刷新数据
+        const arr = Array.from(Array(2), (value, index) => '追加新数据' + (index + 1))
+        // 数据添加到头部
+        this.articles.unshift(...arr)
+        // 手动关闭正在加载的状态
+        this.downLoading = false
+        this.successText = `更新了${arr.length}条数据`
+      }, 1000)
     }
   }
 }
