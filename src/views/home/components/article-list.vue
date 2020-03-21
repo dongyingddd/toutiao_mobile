9b47054af24874e25ce46bd7910d12d5d6f074d0
dongying <2273010094@qq.com>
2020-03-21 14:31:34 +0800

    article-list实现上拉加载

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -6,8 +6,10 @@
   <div class="scroll-wrapper">
       <!-- 文章列表 -->
       <!-- van-list组件 如果不加干涉,初始化完毕后,就会检测自己距离底部的长度,如果超过了限定,就会执行load事件 自动把绑定的loadind变为true -->
-      <van-list v-model="upLoading" :finished="finished" @load="onload">
-
+      <van-list v-model="upLoading" :finished="finished" @load="onload" finished-text="没有了">
+          <van-cell-group>
+              <van-cell title="美股又熔断了" :value="`天台排队${item}`" v-for="item in articles" :key="item"></van-cell>
+          </van-cell-group>
       </van-list>
   </div>
 </template>
@@ -17,18 +19,28 @@ export default {
   data () {
     return {
       upLoading: false, // 表示是否开启了上拉加载
-      finished: false // 表示是否已经完成所有数据的加载
+      finished: false, // 表示是否已经完成所有数据的加载
+      articles: [] // 定义一个数组
     }
   },
   methods: {
     onload () {
       console.log('开始加载数据')
       // 如果你有数据,应该把数据加到list中
-
+      if (this.articles.length > 50) {
+        this.finished = true // 关闭加载
+      } else {
+        // Array.from()方法就是将一个类数据对象或者可遍历对象转换成一个真正的数组
+        const arr = Array.from(Array(15), (value, index) => this.articles.length + index + 1)
+        // 上拉数据 不是覆盖之前的数据 应该把数据追加到数组的尾部
+        this.articles.push(...arr)
+        // 添加完数据 需要手动关掉loading
+        this.upLoading = false
+      }
       // 如果你想关掉
-      setTimeout(() => {
-        this.finished = true // 表示数据已经加载完毕了 没有数据了
-      }, 1000)
+      //   setTimeout(() => {
+      //     this.finished = true // 表示数据已经加载完毕了 没有数据了
+      //   }, 1000)
     }
   }
 }
